322398a750a8dce5533110de80e5f2b968236b95
Jesse Breneman <jbreneman@ologie.com>
2018-03-21 17:28:55 -0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-background">
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Work Sans', sans-serif;
+}
+
+*, *:before, *:after{
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.page-background {
+	padding: 2rem;
+	min-width: 100%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-image: radial-gradient(ellipse at 100% 100%, rgba(38, 13, 119, 0.89) 0%, transparent 50%), radial-gradient(ellipse at 70% 0, rgba(76, 162, 113, 0.52) 0%, transparent 50%), radial-gradient(ellipse at 30% 100%, rgba(102, 13, 119, 0.81) 0%, transparent 50%), radial-gradient(ellipse at 10% 0, rgba(38, 45, 119, 0.94) 0%, transparent 50%), linear-gradient(#26788D, #26788D);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,109 @@
+<template>
+	<section class="container">
+		<h1 class="h1">Jesse Breneman</h1>
+		<h2 class="h2">Front-end web developer, Vue aficionado, tea fiend, robot...</h2>
+		<h3 class="h3">Links to my stuff</h3>
+
+		<ul class="linklist">
+			<li class="link-item" v-for="item in linkItems" :key="item.name">
+				<a :href="item.url" class="link">{{ item.name }}</a>
+			</li>
+		</ul>
+
+	</section>
+</template>
+
+<script>
+export default {
+  data() {
+	return {
+		linkItems: [
+			{
+				name: 'Github',
+				url: 'https://github.com/jbreneman'
+			},
+			{
+				name: 'suggestmycolors',
+				url: 'http://projects.jessebreneman.com/suggest/'
+			},
+			{
+				name: 'background gradient builder',
+				url: 'https://background-gradient-builder.netlify.com/'
+			},
+			{
+				name: '(tea)timer (a PWA)',
+				url: 'https://teatimer.website/'
+			},
+			{
+				name: 'lazuli.js',
+				url: 'https://www.npmjs.com/package/@jbreneman/lazuli-js'
+			}
+		]
+	};
+  }
+}
+</script>
+
+
+<style scoped>
+.container {
+	width: 100%;
+	height: 100%;
+	max-width: 800px;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	mix-blend-mode: overlay;
+}
+
+.h1 {
+	color: white;
+	font-size: 4rem;
+	margin-bottom: .5rem;
+	text-align: center;
+}
+
+.h2 {
+	color: white;
+	font-size: 1.5rem;
+	margin-bottom: 4rem;
+	text-align: center;
+}
+
+.h3 {
+	color: white;
+	font-size: 1.25rem;
+	text-align: center;
+	margin-bottom: 2rem;
+}
+
+.linklist {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	list-style-type: none;
+	padding: 0;
+}
+
+.link-item {
+	display: flex;
+	margin-bottom: 1rem;
+}
+
+.link {
+	mix-blend-mode: overlay;
+	background-color: white;
+	color: rgba(0, 0, 0, .75);
+	padding: .5rem;
+	margin: 0 .5rem;
+	text-decoration: none;
+	border-radius: 1px;
+}
+
+.link:hover {
+	color: white;
+	background-color: rgba(0, 0, 0, .75);
+}
+</style>
